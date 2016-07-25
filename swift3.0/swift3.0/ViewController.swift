938ee77340e754f61e55edea118d6c49c4aeabde
jzkj <chenxy@jingzhaokeji.com>
2016-07-25 14:57:33 +0800

    初始化项目

---
+++ swift3.0/swift3.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift3.0
+//
+//  Created by jzkj on 16/7/25.
+//  Copyright © 2016年 cc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
